32f0e18ff486b19abc0907ec0e44df0ac1954196
ibrahim Adnan <ibrahim@tailored-ink.com>
2020-01-24 02:19:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ if (string===string.toLowerCase()){
 } else if (string===string.toUpperCase()){
   return "YES INDEED!"
 }
+ else if (string==="I love you, Grandma."){
+  return "I love you, too."
+}
 }

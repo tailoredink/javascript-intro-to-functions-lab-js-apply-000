628cb9d213279dd9969d144d88e9e1dac81a1142
ibrahim Adnan <ibrahim@tailored-ink.com>
2020-01-24 02:05:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) 
 if (string===string.toLowerCase()){
   return "i cant hear you!"
-} elseif 
+} else if 
 }
 function sayHiToGrandma(string) {
   if (string===string.toUpperCase()){
